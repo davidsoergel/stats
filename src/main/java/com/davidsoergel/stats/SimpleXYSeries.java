de17561141763762a6bb966821bf3374e1cd8183
David Soergel <dev@davidsoergel.com>
2006-09-14 02:51:00 +0000

    Jandy schema and UI reshuffle

--- src/main/java/com/davidsoergel/stats/SimpleXYSeries.java
+++ src/main/java/com/davidsoergel/stats/SimpleXYSeries.java
@@ -10,88 +10,108 @@ import java.util.List;
  * @author lorax
  * @version 1.0
  */
-public class SimpleXYSeries {
-    private static Logger logger = Logger.getLogger(SimpleXYSeries.class);
+public class SimpleXYSeries
+	{
+	private static Logger logger = Logger.getLogger(SimpleXYSeries.class);
 
-    private List<XYPoint> points = new ArrayList<XYPoint>();
+	private List<XYPoint> points = new ArrayList<XYPoint>();
 
-    private double maxX;
-    private double maxY;
+	private double maxX;
+	private double maxY;
 
-    private class XYPoint {
-        public double x, y;
+	private class XYPoint
+		{
+		public double x, y;
 
-        public XYPoint(double x, double y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+		public XYPoint(double x, double y)
+			{
+			this.x = x;
+			this.y = y;
+			}
+		}
 
-    public void addOrUpdate(double x, double y) {
-        points.add(new XYPoint(x, y));
-        if (x > maxX) {
-            maxX = x;
-        }
-        if (y > maxY) {
-            maxY = y;
-        }
-    }
+	public void addPoint(double x, double y)
+		{
+		points.add(new XYPoint(x, y));
+		if (x > maxX)
+			{
+			maxX = x;
+			}
+		if (y > maxY)
+			{
+			maxY = y;
+			}
+		}
 
-    public int size() {
-        return points.size();
-    }
+	public int size()
+		{
+		return points.size();
+		}
 
-    public double getX(int i) {
-        return points.get(i).x;
-    }
+	public double getX(int i)
+		{
+		return points.get(i).x;
+		}
 
-    public double getY(int i) {
-        return points.get(i).y;
-    }
+	public double getY(int i)
+		{
+		return points.get(i).y;
+		}
 
-    public double getMaxX() {
-        return maxX;
-    }
+	public double getMaxX()
+		{
+		return maxX;
+		}
 
-    public double getMaxY() {
-        return maxY;
-    }
+	public double getMaxY()
+		{
+		return maxY;
+		}
 
-    public double[] getXArray() {
-        double[] result = new double[points.size()];
-        int i = 0;
-        for (XYPoint p : points) {
-            result[i] = p.x;
-            i++;
-        }
-        return result;
-    }
+	public double[] getXArray()
+		{
+		double[] result = new double[points.size()];
+		int i = 0;
+		for (XYPoint p : points)
+			{
+			result[i] = p.x;
+			i++;
+			}
+		return result;
+		}
 
-    public Iterator getYiterator() {
-        return new Iterator() {
-            int trav = 0;
+	public Iterator getYiterator()
+		{
+		return new Iterator()
+		{
+		int trav = 0;
 
-            public boolean hasNext() {
-                return trav < points.size();
-            }
+		public boolean hasNext()
+			{
+			return trav < points.size();
+			}
 
-            public Object next() {
-                return points.get(trav++).y;
-            }
+		public Object next()
+			{
+			return points.get(trav++).y;
+			}
 
-            public void remove() {
-                //To change body of implemented methods use File | Settings | File Templates.
-            }
-        };
-    }
+		public void remove()
+			{
+			//To change body of implemented methods use File | Settings | File Templates.
+			}
+		};
+		}
 
-    public double[] getYArray() {
-        double[] result = new double[points.size()];
-        int i = 0;
-        for (XYPoint p : points) {
-            result[i] = p.y;
-            i++;
-        }
-        return result;
-    }
-}
+	public double[] getYArray()
+		{
+		double[] result = new double[points.size()];
+		int i = 0;
+		for (XYPoint p : points)
+			{
+			result[i] = p.y;
+			i++;
+			}
+		return result;
+		}
+	}
