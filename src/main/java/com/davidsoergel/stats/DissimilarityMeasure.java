e4ac26b2538634e595f721f3c45c3cdee3cd977f
David Soergel <dev@davidsoergel.com>
2008-11-05 22:39:22 +0000

    SVM implementation, and clarifying distinctions between dissimilarities, distances, mappings, and kernels

---
+++ src/main/java/com/davidsoergel/stats/DissimilarityMeasure.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2001-2008 David Soergel
+ * 418 Richmond St., El Cerrito, CA  94530
+ * dev@davidsoergel.com
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the names of any contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package com.davidsoergel.stats;
+
+
+/**
+ * A measure of dissimilarity between points of the generic type.  Note the dissimiliarity need not be symmetric or
+ * linear, and may not honor the triangle inequality; so there is no guarantee that it is a metric or that there exists
+ * an associated dot product.  Really the only constraints are that an object should have zero dissimilarity from
+ * itself, and that all dissimilarities are non-negative.
+ *
+ * @author <a href="mailto:dev.davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public interface DissimilarityMeasure<T>
+	{
+	// -------------------------- OTHER METHODS --------------------------
+
+	/**
+	 * Computes the distance from the first given point to the second.
+	 *
+	 * @param a the starting point of type T
+	 * @param b the ending point of type T
+	 * @return the double distance according to this measure.
+	 */
+	//public String getName();
+	double distanceFromTo(T a, T b);
+
+	/**
+	 * Provides some description of this distance measure, such as its name.
+	 *
+	 * @return a String describing this distance measure.
+	 */
+	String toString();
+	}
---
+++ src/main/java/com/davidsoergel/stats/DistanceMetric.java
@@ -0,0 +1,20 @@
+package com.davidsoergel.stats;
+
+/**
+ * A dissimilarity measure that is also a distance in an inner product space, i.e. it is linear and honors the triangle
+ * inequality.
+ *
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public interface DistanceMetric<T> extends DissimilarityMeasure<T>
+	{
+	/**
+	 * must be symmetric
+	 *
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	double distanceFromTo(T a, T b);
+	}
